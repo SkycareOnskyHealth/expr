97040e4afe65e2c71cd9a11dc68e3cacb8d750de
Phạm Nguyên Cát <dtpham258@gmail.com>
2019-12-26 15:57:12 +0700

    Revert "update"
    This reverts commit 697a69f526b48a36f1b24036e30efe6c0c40f617.

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/onskycloud/expr
 
 go 1.13
-
-require github.com/onskycloud/io v0.0.0-20191031085653-aaa4f3dd15da // indirect
--- main.go
+++ main.go
@@ -1,49 +1,23 @@
-package errors
+package expr
 
 import (
-	"fmt"
-	"strings"
+	"bytes"
 
-	"github.com/onskycloud/io"
+	"text/template"
 )
 
 // ParseText parse template
-// func New(locale Locale, errorType ErrorType, errorCode Status, key string, messages interface{}) (string, error) {
-
-// }
-func (e Errors) detectLocale(locale Locale) ([]ErrorDictionary, error) {
-	fileName := "en_us"
-	if locale != ConvertLocale(fileName) {
-		fileName = strings.ToLower(locale.String())
-	}
-	path := fmt.Sprintf("%s/%s.yaml", RootPath, fileName)
-	file, err := io.ReadAll(path)
-	if err != nil || file == "" {
-		return err
-	}
-	return file
-}
-
-func LoadErrorList() ([]Errors, error) {
-	errorDicts := []Errors{}
-	files, err := io.ReadDir(RootPath)
-	fmt.Printf("files : %+v\n", files)
+func ParseText(name string, body string, data interface{}) (string, error) {
+	_template := template.New(name)
+	_templateParse, err := _template.Parse(body)
 	if err != nil {
-		return errorDicts, err
+		return "", err
 	}
-	for i := 0; i < len(files); i++ {
-		file := &[]ErrorDictionary{}
-		path := fmt.Sprintf("%s/%s", RootPath, files[i].Name)
-		err := io.ReadYamlFile(path, file)
-		if err != nil || file == nil {
-			continue
-		}
-		locale := strings.Split(files[i].Name, ".")[0]
-		errorDict := Errors{
-			Locale:    ConvertLocale(locale),
-			ErrorList: *file,
-		}
-		errorDicts = append(errorDicts, errorDict)
+	var t = template.Must(_templateParse, nil)
+	buf := bytes.Buffer{}
+	err = t.Execute(&buf, data)
+	if err != nil {
+		return "", err
 	}
-	return errorDicts, nil
+	return buf.String(), nil
 }
