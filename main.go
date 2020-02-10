99609dcccb999986be5eaa33ea7bc62da8eb117a
Phạm Nguyên Cát <dtpham258@gmail.com>
2020-02-10 10:11:16 +0700

    add parse file from a path

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package expr
 
 import (
 	"bytes"
+	"os"
+	"path/filepath"
 
 	"text/template"
 )
@@ -16,6 +18,26 @@ func ParseText(name string, body string, data interface{}) (string, error) {
 	var t = template.Must(_templateParse, nil)
 	buf := bytes.Buffer{}
 	err = t.Execute(&buf, data)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+// ParseFile parse template
+func ParseFile(path string, data interface{}) (string, error) {
+	wd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	fp := filepath.Join(wd, path)
+
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		return "", err
+	}
+	buf := new(bytes.Buffer)
+	err = tmpl.Execute(buf, data)
 	if err != nil {
 		return "", err
 	}
